8b950a123ee3204c7ad0f1a7d0aaed72869d4468
bb3531 <3531272205@qq.com>
2026-06-08 00:08:54 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+var C='av4';self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(['/','/index.html'])}).catch(function(){}))});self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
